0de07427d9862aadb3ed611a8a20d70dba1d95ba
Carole <cjpillette@hotmail.com>
2017-07-06 12:51:30 +1000

    alert selected day from the calendar

--- react-web/src/pages/HomePage.js
+++ react-web/src/pages/HomePage.js
@@ -5,43 +5,52 @@ import InfiniteCalendar from 'react-infinite-calendar'
 import 'react-infinite-calendar/styles.css'
 import TimeSlotModal from '../components/TimeSlotModal'
 
-const HomePage = ({
-    // props
-}) => {
-  var today = new Date();
-  var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+class HomePage extends React.Component {
+    // state
 
-  return (
-    <div>
-        <TimeSlotModal />
-        <InfiniteCalendar
-          width={400}
-          height={600}
-          selected={today}
-          disabledDays={[0,6]}
-          minDate={lastWeek}
-          locale={{
-            weekStartsOn: 1
-          }}
-          theme={{
-              selectionColor: date => {
-                return moment(date).isBefore(today) ? '#EC6150' : '#559FFF'
-              },
-              textColor: {
-                default: '#333',
-                active: '#FFF'
-              },
-              weekdayColor: 'rgb(250, 201, 0)',
-              headerColor: 'rgb(243, 134, 18)',
-              floatingNav: {
-                background: 'rgba(243, 134, 18, 0.96)',
-                color: '#FFF',
-                chevron: '#000'
-              }
+
+//functions
+onStandaloneSelect = (value) => {
+  alert(value);
+}
+
+
+// rendering stuff for the user
+  render() {
+    const today = new Date()
+    const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7)
+    return (
+      <div>
+          <TimeSlotModal />
+          <InfiniteCalendar
+            width={400}
+            height={600}
+            selected={today}
+            onSelect={this.onStandaloneSelect}
+            minDate={lastWeek}
+            locale={{
+              weekStartsOn: 1
             }}
-        />
-    </div>
-  )
+            theme={{
+                selectionColor: date => {
+                  return moment(date).isBefore(today) ? '#EC6150' : '#559FFF'
+                },
+                textColor: {
+                  default: '#333',
+                  active: '#FFF'
+                },
+                weekdayColor: 'rgb(250, 201, 0)',
+                headerColor: 'rgb(243, 134, 18)',
+                floatingNav: {
+                  background: 'rgba(243, 134, 18, 0.96)',
+                  color: '#FFF',
+                  chevron: '#000'
+                }
+              }}
+          />
+      </div>
+    )
+  }
 }
 
 
