20db8384724cb5ba6810f2a5f0fe06b45f4fda90
Carole <cjpillette@hotmail.com>
2017-07-11 16:54:46 +1000

    fixed bug in booking form that didnt pass startTime to booking

--- react-web/src/components/Booking.js
+++ react-web/src/components/Booking.js
@@ -17,7 +17,6 @@ export default function Booking({
   return (
     <article>
       <p>On { moment(dateSelected).format('ddd, DD-MM-YYYY') }</p>
-      {"XXYY".slice(0, 2) + "/" + "XXYY".slice(2)}
       <p>at { startTimeSelected }</p>
       <p>{ inspectionType }</p>
       <Link to={ `/bookings/${_id}` }>
