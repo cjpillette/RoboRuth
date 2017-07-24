9eb52de5134a18f11eb0fdd9d244506250f65e59
Carole <cjpillette@hotmail.com>
2017-07-24 11:03:47 +1000

    reconfigured atlas and compass and seeded database

--- api/db/seed.js
+++ api/db/seed.js
@@ -2,9 +2,8 @@ const Path = require('path')
 const envPath = Path.join(__dirname, '..', '.env')
 require('dotenv').config({ path: envPath })
 
-const mongoose = require('./db')
-
 const User = require('../models/user')
+const Aqp = require('../models/aqp')
 
 const user = new User({
   role: 'Admin',
@@ -15,34 +14,48 @@ const user = new User({
   isArchived: false,
   aqp: null
 })
-User.register(user, 'YOUR_BOSSES_PASSWORD', (error, user) => {
+
+
+User.register(user, process.env.EWEN_PASSWORD, (error, user) => {
   // Error in registration
   if (error) {
     // Our middleware failed with this error
+    console.log(error)
     return
   }
+
+  console.log(user)
 })
 
-// User.create([
-//   {
-//     role: 'Admin',
-//     firstName: 'Ewen',
-//     lastName: 'Davey',
-//     email: 'ewen@test.com',
-//     phoneNumber: '0361653777',
-//     isArchived: false,
-//     aqp: null
-//   },
-//   {
-//     role: 'Client',
-//     firstName: 'David',
-//     lastName: 'Cusato',
-//     email: 'david@test.com',
-//     phoneNumber: '0391923435',
-//     isArchived: false,
-//     aqp: null
-//   }
-// ])
+Aqp.create([
+  {
+    businessName: 'Cusato Garlic',
+    aqpNumber: 'BT342'
+  },
+  {
+    businessName: 'Fresh Fruits A2',
+    aqpNumber: 'BT355'
+  },
+  {
+    businessName: 'Reid Fruits',
+    aqpNumber: 'BT367'
+  },
+  {
+    businessName: 'Mongoon Imports',
+    aqpNumber: 'BT543'
+  },
+  {
+    businessName: 'Huon Aquaculture',
+    aqpNumber: 'BT142'
+  }
+])
+.then(result => {
+  console.log(result)
+})
+.catch(err => {
+  console.log(err)
+})
+
 //   .then(() => {
 //     mongoose.disconnect();
 //   })
